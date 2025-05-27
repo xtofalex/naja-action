1b5e8b8d587150a5317690a31f1644812283f527
xtof <christophe.alex@gmail.com>
2025-05-28 00:12:06 +0200

    more printing

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ VERILOG_FILE="$ROOT/design.v"
 
 echo "Running Yosys on: $ROOT/$YOSYS_SCRIPT"
 echo "Design root: $ROOT/$SYNTH_ROOT"
+echo "Liberty file: $LIBERTY_FILE"
+echo "Verilog file: $VERILOG_FILE"
 
 export SYNTH_ROOT="$ROOT/$SYNTH_ROOT"
 /yosys-install/bin/yosys "$YOSYS_SCRIPT"
