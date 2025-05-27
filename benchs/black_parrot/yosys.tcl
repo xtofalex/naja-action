f9543ab1d643984a3f989a52f69e7ab629d39ea8
xtof <christophe.alex@gmail.com>
2025-05-28 01:19:57 +0200

    change synth

--- benchs/black_parrot/yosys.tcl
+++ benchs/black_parrot/yosys.tcl
@@ -8,7 +8,22 @@ read_liberty -lib $::env(SYNTH_ROOT)/libs/fakeram45_256x95.lib
 read_liberty -lib $::env(SYNTH_ROOT)/libs/fakeram45_512x64.lib 
 read_verilog $::env(SYNTH_ROOT)/black_parrot.v $::env(SYNTH_ROOT)/macros.v
 keep_hierarchy
-synth
+
+synth -run :fine 
+
+# Get rid of indigestibles
+chformal -remove
+delete t:\$print
+
+# rename registers to have the verilog register name in its name
+# of the form \regName$_DFF_P_. We should fix yosys to make it the reg name.
+# At least this is predictable.
+renames -wire
+
+log_cmd abc
+
+# Optimize the design
+opt -purge
 
 # Replace undef values with defined constants
 setundef -zero
@@ -39,4 +54,4 @@ dfflibmap -liberty $::env(SYNTH_ROOT)/libs/NangateOpenCellLibrary_typical.lib
 # Remove unused cells and wires
 opt_clean -purge
 
-write_verilog synth.v
+write_verilog -nohex -nodec synth.v
