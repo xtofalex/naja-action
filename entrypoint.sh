70875abeee96edbb18882f7214c07d4fbc29b8b5
xtof <christophe.alex@gmail.com>
2025-05-27 23:33:53 +0200

    fix paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
+ROOT="/github/workspace"
 YOSYS_SCRIPT="$1"
 SYNTH_ROOT="$2"
 
-echo "Running Yosys on: $YOSYS_SCRIPT"
-echo "Design root: $SYNTH_ROOT"
+echo "Running Yosys on: $ROOT/$YOSYS_SCRIPT"
+echo "Design root: $ROOT/$SYNTH_ROOT"
 
 ls -l "$YOSYS_SCRIPT"
 ls -l "$SYNTH_ROOT"
