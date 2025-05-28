e623198f0734d15adf42be7fb56dc66768f84f6b
xtof <christophe.alex@gmail.com>
2025-05-28 17:04:17 +0200

    list content

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,14 @@ SYNTH_ROOT="$2"
 #LIBERTY_FILE="$ROOT/$SYNTH_ROOT/libs/NangateOpenCellLibrary_typical.lib"
 VERILOG_FILE="synth.v"
 
-echo "Running Yosys on: $ROOT/$YOSYS_SCRIPT"
-echo "Design root: $ROOT/$SYNTH_ROOT"
-echo "Liberty file: $LIBERTY_FILE"
-echo "Verilog file: $VERILOG_FILE"
+#echo "Running Yosys on: $ROOT/$YOSYS_SCRIPT"
+#echo "Design root: $ROOT/$SYNTH_ROOT"
+#echo "Liberty file: $LIBERTY_FILE"
+#echo "Verilog file: $VERILOG_FILE"
 
 #export SYNTH_ROOT="$ROOT/$SYNTH_ROOT"
 #/yosys-install/bin/yosys "$YOSYS_SCRIPT"
-cd /najaeda-or/flow && make
+cd /najaeda-or/flow && ls && ls scripts
+cd /majaeda-or/flow && make
 
 #python3 $ROOT/scripts/count_leaves.py --liberty "$LIBERTY_FILE" --verilog "$VERILOG_FILE" 
