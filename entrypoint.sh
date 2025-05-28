f07cf41c3f9e64e2afc908f5c5e1b28cb7f021ca
xtof <christophe.alex@gmail.com>
2025-05-28 16:27:31 +0200

    show flow home

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,12 @@ SYNTH_ROOT="$2"
 #LIBERTY_FILE="$ROOT/$SYNTH_ROOT/libs/NangateOpenCellLibrary_typical.lib"
 VERILOG_FILE="synth.v"
 
+echo "FLOW_HOME: $FLOW_HOME"
 echo "Running Yosys on: $ROOT/$YOSYS_SCRIPT"
 echo "Design root: $ROOT/$SYNTH_ROOT"
 echo "Liberty file: $LIBERTY_FILE"
 echo "Verilog file: $VERILOG_FILE"
+
 
 #export SYNTH_ROOT="$ROOT/$SYNTH_ROOT"
 #/yosys-install/bin/yosys "$YOSYS_SCRIPT"
