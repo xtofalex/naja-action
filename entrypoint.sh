1cc3c11a8f291035b052f5f37b485bd3f2f8636a
xtof <christophe.alex@gmail.com>
2025-05-28 15:46:31 +0200

    switch to or scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 ROOT="/github/workspace"
 YOSYS_SCRIPT="$1"
 SYNTH_ROOT="$2"
-LIBERTY_FILE="$ROOT/$SYNTH_ROOT/libs/NangateOpenCellLibrary_typical.lib"
+#LIBERTY_FILE="$ROOT/$SYNTH_ROOT/libs/NangateOpenCellLibrary_typical.lib"
 VERILOG_FILE="synth.v"
 
 echo "Running Yosys on: $ROOT/$YOSYS_SCRIPT"
@@ -12,7 +12,9 @@ echo "Design root: $ROOT/$SYNTH_ROOT"
 echo "Liberty file: $LIBERTY_FILE"
 echo "Verilog file: $VERILOG_FILE"
 
-export SYNTH_ROOT="$ROOT/$SYNTH_ROOT"
-/yosys-install/bin/yosys "$YOSYS_SCRIPT"
+#export SYNTH_ROOT="$ROOT/$SYNTH_ROOT"
+#/yosys-install/bin/yosys "$YOSYS_SCRIPT"
 
-python3 $ROOT/scripts/count_leaves.py --liberty "$LIBERTY_FILE" --verilog "$VERILOG_FILE" 
+cd /najaeda-or/flow && make
+
+#python3 $ROOT/scripts/count_leaves.py --liberty "$LIBERTY_FILE" --verilog "$VERILOG_FILE" 
