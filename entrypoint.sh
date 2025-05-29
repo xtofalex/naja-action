95ba613445487b59973f8f76bda1fb37a8046d15
xtof <christophe.alex@gmail.com>
2025-05-29 13:07:55 +0200

    print python version

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,10 @@ VERILOG_FILE="synth.v"
 #/yosys-install/bin/yosys "$YOSYS_SCRIPT"
 export DESIGN_HOME="$ROOT/$DESIGN_HOME"
 export DESIGN_CONFIG="$ROOT/$DESIGN_CONFIG"
-python3 -c "import yaml; print(yaml.__version__)"
-python -c "import yaml; print(yaml.__version__)"
+echo $(which python3)
+echo $(which python)
+echo $(python3 -c "import yaml; print(yaml.__version__)")
+echo $(python -c "import yaml; print(yaml.__version__)")
 cd /najaeda-or/flow && make
 
 #python3 $ROOT/scripts/count_leaves.py --liberty "$LIBERTY_FILE" --verilog "$VERILOG_FILE" 
