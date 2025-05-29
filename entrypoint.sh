fca827a7357bdd8e0660327e44dd6c4894a1782e
xtof <christophe.alex@gmail.com>
2025-05-29 12:08:43 +0200

    new test

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@ VERILOG_FILE="synth.v"
 #/yosys-install/bin/yosys "$YOSYS_SCRIPT"
 export DESIGN_HOME="$ROOT/$DESIGN_HOME"
 export DESIGN_CONFIG="$ROOT/$DESIGN_CONFIG"
+python3 -c "import yaml; print(yaml.__version__)"
 python -c "import yaml; print(yaml.__version__)"
 cd /najaeda-or/flow && make
 
