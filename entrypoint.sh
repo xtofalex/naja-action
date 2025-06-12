e94b647d180c0ab75c096bbeaf0a14afcaa8d61d
xtof <christophe.alex@gmail.com>
2025-06-12 14:05:50 +0200

    read Tcl in Yosys

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,6 @@ DESIGN_CONFIG="$3"
 export YOSYS_EXE="/yosys/bin/yosys"
 export DESIGN_CONFIG="$ROOT/$DESIGN_CONFIG"
 
-echo "Environment variables:"
-env
-
 if [ "$MODE" == "orfs" ]; then
     echo "Running in ORFS mode"
     export DESIGN_HOME="$ROOT/$DESIGN_HOME"
@@ -25,9 +22,13 @@ if [ "$MODE" == "orfs" ]; then
     python3 $ROOT/scripts/count_leaves.py --primitives_mode="liberty" --liberty "$LIBERTY_FILE" --verilog "$VERILOG_FILE" 
 else
     echo "Running in direct yosys mode"
+    echo "Yosys executable: $YOSYS_EXE"
+    echo "Yosys version: $($YOSYS_EXE -V)"
+    echo "Design home: $DESIGN_HOME"
+    echo "Design config: $DESIGN_CONFIG"
     #export SYNTH_ROOT="$ROOT/najaeda-or/flow"
     #export SYNTH_ROOT="$ROOT/$SYNTH_ROOT"
-    cd $DESIGN_HOME && $YOSYS_EXE $DESIGN_CONFIG
+    cd $DESIGN_HOME && $YOSYS_EXE -c $DESIGN_CONFIG
     VERILOG_FILE="naja_netlist.v"
     echo "Verilog file: $VERILOG_FILE"
 
